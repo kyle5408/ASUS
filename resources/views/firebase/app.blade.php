8d9ee4c12be6f3aad61a56826f68832b1fdea366
kyle5408 <kyle5408@yahoo.com.tw>
2021-12-15 11:07:33 +0800

    feat: add navbar and index view

---
+++ resources/views/firebase/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Firebase</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  @include('firebase.inc.navbar')
+  <div class="py-3">
+    @yield('content')
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  
+</body>
+
+</html>
---
+++ resources/views/firebase/inc/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+
+  <div class="container">
+    <a class="navbar-brand" href="#">Firebase</a>
+  
+
+    <div class="navbar-content">
+      <ul class=" ms-auto mb-2 mb-lg-0 d-flex">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ url('contact') }}">Contact</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Register</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Login</a>
+        </li>
+
+    </div>
+</nav>
